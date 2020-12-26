901c05be63b9e46df48ae86dfffd0c96e626ad74
Rob J Goedman <goedman@mac.com>
2020-12-26 16:11:05 -0700

    Rel 1.2.7

---
+++ research/kwargs.jl
@@ -0,0 +1,18 @@
+using Pkg, DrWatson
+@quickactivate "StatisticalRethinkingStan"
+
+function tf1(f::Number; l=12, kwargs...)
+    x = range(f, stop=l, length=10)
+    y = 10rand(length(x))
+    println(length(keys(kwargs)))
+    if length(keys(kwargs)) > 0
+        plot(;kwargs...)
+        plot!(x, y)
+    else
+        plot(x, y)
+    end
+end
+
+tf1(1)
+tf1(3)
+tf1(5; xlims=(6, 9), ylims=(0, 20))
