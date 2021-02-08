e4fc5a79bad79d3881468fdb54ff07330426821c
Rob J Goedman <goedman@mac.com>
2021-02-08 09:39:20 -0700

    Start ch8

--- notebooks/07/clip-07-35s.jl
+++ notebooks/07/clip-07-35s.jl
@@ -83,6 +83,9 @@ begin
 	waic(lp5_1s_t)
 end
 
+# ╔═╡ 3a9ff05e-6a16-11eb-2ba0-530fb4bc18f4
+size(mu5_1s_t)
+
 # ╔═╡ 8f107504-68ed-11eb-002b-372884f598b0
 stan5_2_t = "
 data {
@@ -222,7 +225,7 @@ begin
 		xlab="PSIS Pareto k", ylab="WAIC penalty", leg=false)
 	vline!([0.5])
 	for state in [13, 20, 34, 44, 50]
-		annotate!([([pk5_1s_t[state] + 0.12], [waic_5_1s_pw_t.penalty[state] + 0.3],
+		annotate!([([pk5_1s_t[state] + 0.02], [waic_5_1s_pw_t.penalty[state]],
 			Plots.text(df[state, :Loc], 6, :red, :right))])
 	end
 	plot!()
@@ -242,7 +245,7 @@ begin
 		xlab="PSIS Pareto k", ylab="WAIC penalty", leg=false)
 	vline!([0.5])
 	for state in [13, 20, 34, 44, 50]
-		annotate!([([pk5_3s_t[state] + 0.15], [waic_5_3s_pw_t.penalty[state] + 0.02],
+		annotate!([([pk5_3s_t[state] + 0.02], [waic_5_3s_pw_t.penalty[state]],
 			Plots.text(df[state, :Loc], 6, :red, :right))])
 	end
 	plot!()
@@ -264,7 +267,7 @@ begin
 		xlab="PSIS Pareto k", ylab="WAIC penalty", leg=false)
 	vline!([0.5])
 	for state in [13, 20, 34, 44, 50]
-		annotate!([([pk5_2s_t[state] + 0.1], [waic_5_2s_pw_t.penalty[state] + 0.02],
+		annotate!([([pk5_2s_t[state] + 0.01], [waic_5_2s_pw_t.penalty[state] + 0.01],
 			Plots.text(df[state, :Loc], 6, :red, :right))])
 	end
 	plot!()
@@ -282,6 +285,7 @@ md" ## End of clip-07-35s.jl"
 # ╠═4c482dfa-68ed-11eb-2db9-a54ab816beb7
 # ╠═806d4352-69b7-11eb-00a5-791917fa9d45
 # ╠═6210f1ba-68ef-11eb-1d55-653d1c8a147c
+# ╠═3a9ff05e-6a16-11eb-2ba0-530fb4bc18f4
 # ╠═8f107504-68ed-11eb-002b-372884f598b0
 # ╠═cbb9ef56-68ed-11eb-387b-252ff6dc5b21
 # ╠═5d216004-69b7-11eb-34d4-8be76d4ae47c
